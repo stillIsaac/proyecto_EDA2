78d28cf331f5decdcdfccea7f3cb7ec78eef5a45
stillIsaac <95540706+stillIsaac@users.noreply.github.com>
2023-08-24 16:45:13 -0500

    Update ejercito.h

--- Codigo/ejercito.h
+++ Codigo/ejercito.h
@@ -1,8 +1,31 @@
 #ifndef EJERCITO_H
 #define EJERCITO_H
 
+class ejercito {
+  public:
+    // Getter for 'tipo'
+    char getTipo() const {
+        return tipo[0];
+    }
 
+    // Setter for 'tipo'
+    void setTipo(char nuevoTipo) {
+        tipo[0] = nuevoTipo;
+    }
 
+    // Getter for 'cant'
+    int getCant() const {
+        return cant;
+    }
 
+    // Setter for 'cant'
+    void setCant(int nuevaCant) {
+        cant = nuevaCant;
+    }
+
+  private:
+    char tipo[1];
+    int cant;
+};
 
 #endif
