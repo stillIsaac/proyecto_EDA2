8be5f614a72ba38d91418f7a500c36b697d99ed7
stillIsaac <95540706+stillIsaac@users.noreply.github.com>
2023-08-24 16:46:09 -0500

    Update ejercito.cxx

--- Codigo/ejercito.cxx
+++ Codigo/ejercito.cxx
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "ejercito.h"
 
+
+    // Getter for 'tipo'
+    char getTipo() const {
+        return tipo[0];
+    }
+
+    // Setter for 'tipo'
+    void setTipo(char nuevoTipo) {
+        tipo[0] = nuevoTipo;
+    }
+
+    // Getter for 'cant'
+    int getCant() const {
+        return cant;
+    }
+
+    // Setter for 'cant'
+    void setCant(int nuevaCant) {
+        cant = nuevaCant;
+    }
