72cd3fca7f62ee023d6f034d6821dc6517824501
stillIsaac <95540706+stillIsaac@users.noreply.github.com>
2023-08-24 16:52:14 -0500

    Update jugador.h

--- Codigo/jugador.h
+++ Codigo/jugador.h
@@ -1,6 +1,19 @@
 #ifendf JUGADOR_H
 #define JUGADOR_H
 
-class Jugador 
+#include <list>
+#include <ejercito.h>
+#include <tarjetas.h>
+
+class Jugador {
+
+  public:
+
+
+  private:
+    string nombre;
+    string color;
+    list <Ejercito> tropas;
+    
 
 #endif
